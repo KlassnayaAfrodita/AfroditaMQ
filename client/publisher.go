eceb93603595fe3251dd8d8669161c48697dfce7
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-11-17 22:19:29 +0300

    Update publisher.go

--- client/publisher.go
+++ client/publisher.go
@@ -7,22 +7,22 @@ import (
 	"net/http"
 )
 
-// SimplePublisher реализует интерфейс Publisher
-type SimplePublisher struct {
+// ClientPublisher реализует интерфейс ClientPublisher
+type ClientPublisher struct {
 	ClientID string
 	BaseURL  string
 }
 
-// NewPublisher создает новый экземпляр SimplePublisher
-func NewPublisher(clientID string, baseURL string) *SimplePublisher {
-	return &SimplePublisher{
+// NewClientPublisher создает новый экземпляр ClientPublisher
+func NewClientPublisher(clientID string, baseURL string) *ClientPublisher {
+	return &ClientPublisher{
 		ClientID: clientID,
 		BaseURL:  baseURL,
 	}
 }
 
-// Publish публикует сообщение на указанный топик с приоритетом и TTL
-func (p *SimplePublisher) Publish(topic string, message string, priority int, ttl int64) error {
+// Publish публикует сообщение на указанный топик
+func (p *ClientPublisher) Publish(topic, message string, priority int, ttl int64) error {
 	url := fmt.Sprintf("%s/publish", p.BaseURL)
 	publishRequest := map[string]interface{}{
 		"topic":    topic,
@@ -47,5 +47,32 @@ func (p *SimplePublisher) Publish(topic string, message string, priority int, tt
 	}
 
 	fmt.Printf("Message published to topic %s: %s\n", topic, message)
+	return nil
+}
+
+// PublishBatch публикует пакет сообщений на указанный топик
+func (p *ClientPublisher) PublishBatch(topic string, messages []map[string]interface{}) error {
+	url := fmt.Sprintf("%s/publish_batch", p.BaseURL)
+	publishBatchRequest := map[string]interface{}{
+		"topic":    topic,
+		"messages": messages,
+	}
+
+	data, err := json.Marshal(publishBatchRequest)
+	if err != nil {
+		return fmt.Errorf("failed to marshal publish batch request: %v", err)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return fmt.Errorf("failed to send publish batch request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to publish batch, status code: %d", resp.StatusCode)
+	}
+
+	fmt.Printf("Batch of messages published to topic %s\n", topic)
 	return nil
 }
